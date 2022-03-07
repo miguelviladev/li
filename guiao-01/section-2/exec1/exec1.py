fa561f40a4fe78006b4c0907b51561b2d6b70d8b
Miguel Vila <miguel.vila.dev@gmail.com>
2022-03-07 14:51:31 +0000

    exec1 fix

--- guiao-01/section-2/exec1/exec1.py
+++ guiao-01/section-2/exec1/exec1.py
@@ -1,13 +1,14 @@
 import sys
 import hashlib
 
-ficheiros = [nome for nome in sys.argv[1:]]
+ficheiros = sys.argv[1:]
 h = hashlib.sha1()
+conteudo = ''
 
 for ficheiro in ficheiros:
-    conteudo = ''
     with open(ficheiro, 'r') as f:
         conteudo += f.read()
-    h.update(conteudo.replace('\n','').encode('utf-8'))
-    
+h.update(conteudo.encode('utf-8'))
+
 print(h.hexdigest())
+print(conteudo)
