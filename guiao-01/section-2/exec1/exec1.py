75bb2683df1b2a608c0ddfe41478dac76cb3d611
Miguel Vila <miguel.vila.dev@gmail.com>
2022-03-07 14:56:58 +0000

    exec1 fix

--- guiao-01/section-2/exec1/exec1.py
+++ guiao-01/section-2/exec1/exec1.py
@@ -8,7 +8,6 @@ conteudo = ''
 for ficheiro in ficheiros:
     with open(ficheiro, 'r') as f:
         conteudo += f.read()
-h.update(conteudo.encode('utf-8'))
+h.update(conteudo.replace('\n', '').encode('utf-8'))
 
 print(h.hexdigest())
-print(conteudo)
