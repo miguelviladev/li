cdaaa8b858bb2755542579d9887d068250497723
Miguel Vila <miguel.vila.dev@gmail.com>
2022-03-21 15:07:13 +0000

    Exec3

--- guiao02/exec3/exec3.py
+++ guiao02/exec3/exec3.py
@@ -4,4 +4,5 @@ import sys
 proc = Popen('ls -la', stdout=PIPE, shell=True)
 return_code = proc.wait()
 for line in iter(proc.stdout.readline, b''):
-    if sys.argv[1] not in line.decode('utf-8'): print(line.decode('utf-8'), end='')
+    if sys.argv[1] not in line.decode('utf-8'):
+        print(line.decode('utf-8'), end='')
